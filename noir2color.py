9f07a1fa7fa365721b6c53a8384c9db7dc4aadf4
walter_wu090 <walter.wu090@gmail.com>
2017-06-26 22:12:37 -0400

    Implementation of deconvolution layer.
    Also implemented, a computation for deconvolution output shape, if output shape is not provided as an argument

--- noir2color.py
+++ noir2color.py
@@ -2,16 +2,22 @@ import tensorflow as tf
 import numpy as np
 
 
-def conv_pool(x, conv_ksize, conv_stride, pool_ksize,
-              pool_stride, alpha=5, pooling=True, name='conv'):
+def conv_avg_pool(x,
+                  conv_ksize,
+                  conv_stride,
+                  pool_ksize,
+                  pool_stride,
+                  alpha=5,
+                  pooling=True,
+                  name='conv'):
     """Convolution-LReLU-average pooling layers
 
     Args:
         x(Tensor): Input from the previous layer.
-        conv_ksize(list): 4-D array, ksize for the convolution layer.
-        conv_stride(list): 4-D array, stride for the convolution layer.
-        pool_ksize(list): 4-D array, ksize for the average pooling layer.
-        pool_stride(list): 4-D array, stride for the average pooling layer.
+        conv_ksize(list): ksize for the convolution layer.
+        conv_stride(list): stride for the convolution layer.
+        pool_ksize(list): ksize for the average pooling layer.
+        pool_stride(list): stride for the average pooling layer.
         alpha(float): Parameter for Leaky ReLU
         pooling(bool): If set to False, a pooling layer will not be added after the conv
             layer and pooling parameters will be ignored
@@ -26,12 +32,12 @@ def conv_pool(x, conv_ksize, conv_stride, pool_ksize,
         bias = tf.get_variable(name='conv_b', shape=[conv_ksize[-1]],
                                initializer=tf.constant_initializer(0))
 
-        convoluted = tf.nn.conv2d(x, filter=weights, strides=conv_stride, padding='SAME')
+        convoluted = tf.nn.conv2d(x, filter=weights, strides=conv_stride, padding='VALID')
         convoluted = convoluted + bias
         conv = lrelu(convoluted, alpha)
 
         if pooling:
-            conv = tf.nn.avg_pool(conv, ksize=pool_ksize, strides=pool_stride, padding='SAME')
+            conv = tf.nn.avg_pool(conv, ksize=pool_ksize, strides=pool_stride, padding='VALID')
         return conv
 
 
@@ -92,17 +98,51 @@ def fully_conn(x, output_size, name='fc', activation=True):
         return output
 
 
-def deconv(x, ksize, stride, output_size, name='deconv'):
+def deconv(x, ksize, stride, output_shape=None, padding='SAME', name='deconv'):
     """Deconvolution (convolution transpose) layer.
 
     Args:
         x(Tensor): Input tensor from the previous layer.
-        ksize(list): 4-D array, filter size.
-        stride(list): 4-D array, stride size.
-        output_size(list): 1-D array, output size of the deconv layer.
+        ksize(list): filter size.
+        stride(list): stride size.
+        output_shape(list): 1-D array, output size of the deconv layer. Default None,
+            if this argument is left as None, an output shape will be calculated.
+        padding(str): Padding method for the deconvolution, choose between 'SAME' and
+            'VALID', default 'SAME' padding.
         name(str): Name for the variable scope of this layer.
 
     Returns:
         Output tensor.
     """
-    raise NotImplementedError
+    with tf.variable_scope(name):
+        weights = tf.get_variable(name='deconv_w', shape=ksize,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.02))
+        biases = tf.get_variable(name='deconv_b', shape=ksize[-1],
+                                 initializer=tf.constant_initializer(0))
+
+        if output_shape is None:
+            # if output_shape is not provided, compute default value
+            stride_h = stride[1]
+            stride_w = stride[2]
+            input_h = x.get_shape()[1]
+            input_w = x.get_shape()[2]
+            filter_h = ksize[1]
+            filter_w = ksize[2]
+
+            output_shape = [n for n in x.get_shape()]
+            output_shape[-1] = ksize[-1]  # number of kernels
+
+            if padding == 'SAME':
+                output_shape[1] = (input_h + stride_h - 1) // stride_h
+                output_shape[2] = (input_w + stride_w - 1) // stride_w
+            elif padding == 'VALID':
+                output_shape[1] = (input_h + stride_h - filter_h) // stride_h
+                output_shape[2] = (input_w + stride_w - filter_w) // stride_w
+            else:
+                # if padding is not one of 'SAME' and 'VALID', raise an error
+                raise ValueError("Padding must be one of 'SAME' and 'VALID', set to None to use"
+                                 "default padding")
+
+        deconvolved = tf.nn.conv2d_transpose(x, filter=weights, output_shape=output_shape,
+                                             strides=stride, padding='SAME')
+        return deconvolved
